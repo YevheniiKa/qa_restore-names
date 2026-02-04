4cc9ab962436ef7d9f7fba911ab0322040d0aad1
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-02-04 21:55:46 +0200

    added a more tests according to AI comments

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -19,7 +19,7 @@ describe("restoreNames", () => {
 
     restoreNames(users);
 
-    expect(users).toEqual([
+    expect(users).toStrictEqual([
       {
         firstName: "Jack",
         lastName: "Holy",
@@ -29,6 +29,135 @@ describe("restoreNames", () => {
         firstName: "Mike",
         lastName: "Adams",
         fullName: "Mike Adams",
+      },
+    ]);
+  });
+
+  it("users object no need to restore firstName field", () => {
+    const users = [
+      {
+        firstName: "Jack",
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        firstName: "Mike",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toStrictEqual([
+      {
+        firstName: "Jack",
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+      {
+        firstName: "Mike",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+    ]);
+  });
+
+  it("empty users array", () => {
+    const users = [];
+
+    restoreNames(users);
+
+    expect(users).toStrictEqual([]);
+  });
+
+  it("users have mix of failed & normal user objects", () => {
+    const users = [
+      {
+        firstName: "Mike",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+      {
+        firstName: "Alex",
+        lastName: "Brosky",
+        fullName: "Alex Brosky",
+      },
+
+      {
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+      {
+        firstName: "Mike",
+        lastName: "Clerevald",
+        fullName: "Mike Clerevald",
+      },
+      {
+        firstName: undefined,
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toStrictEqual([
+      {
+        firstName: "Mike",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+      {
+        firstName: "Alex",
+        lastName: "Brosky",
+        fullName: "Alex Brosky",
+      },
+
+      {
+        firstName: "Mike",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+      {
+        firstName: "Mike",
+        lastName: "Clerevald",
+        fullName: "Mike Clerevald",
+      },
+      {
+        firstName: "Jack",
+        lastName: "Holy",
+        fullName: "Jack Holy",
+      },
+    ]);
+  });
+
+  it("some firstName is incorrect", () => {
+    const users = [
+      {
+        firstName: "John",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+      {
+        firstName: "Richard",
+        lastName: "Brosky",
+        fullName: "Alex Brosky",
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users).toStrictEqual([
+      {
+        firstName: "Mike",
+        lastName: "Adams",
+        fullName: "Mike Adams",
+      },
+      {
+        firstName: "Alex",
+        lastName: "Brosky",
+        fullName: "Alex Brosky",
       },
     ]);
   });
